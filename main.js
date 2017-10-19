783b6a398b01a0da62a99dfa64429d824db5cc24
Pranshu panwar <pranshupnwr@gmail.com>
2017-10-19 22:42:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Pranshu Panwar on 19-10-2017.
+ */
+var red =[0,100,63];
+var orange=[40,100,60];
+var green=[75,100,40];
+var blue =[196,77,55];
+var purple=[280,50,60];
+var name = "Happy diwali";
+var name1="moti shreya"
+var color =[red,orange,green,blue,purple];
+
+drawName(name,color);
+//drawName(name1,color);
+bounceBubbles();
+
+//bubbleShape="square";
